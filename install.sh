83fa5848ab9505341700ce7c2e1a947c1948dc82
Edder Rojas <edder.rojas@gmail.com>
2014-08-19 00:08:19 -0600

    Added better installation error handling

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
+# execute scripts from bash your server
+# wget -O - https://raw.githubusercontent.com/edderrd/prezto/master/install.sh | zsh
+
 # Uninstall oh my zsh
-uninstall_oh_my_zsh
+
+# uninstall oh-my-zsh
+if ! type "uninstall_oh_my_zsh" &> /dev/null; then
+    echo "Uninstalling oh-my-zsh"
+    uninstall_oh_my_zsh
+fi
+
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "removing remaining oh-my-zsh files"
+    mv ~/.zshrc{,_bk}
+    mv ~/.zprofile{,_bk}
+    rm -vfr ~/.oh-my-zsh > /dev/null
+fi
+
+# start your zsh
+zsh
 
 # Open new shell, open zsh and clone the presto repo
 git clone --recursive https://github.com/edderrd/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
