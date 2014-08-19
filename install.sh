ee233452928f077a0928ec368439025ae54828a5
Edder Rojas <edder.rojas@gmail.com>
2014-08-18 23:52:19 -0600

    Added installation script
    - With uninstall oh-my-zsh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Uninstall oh my zsh
+uninstall_oh_my_zsh
+
+# Open new shell, open zsh and clone the presto repo
+git clone --recursive https://github.com/edderrd/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Get prezto's default configuration
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# Change default shell to zsh
+chsh -s /bin/zsh
